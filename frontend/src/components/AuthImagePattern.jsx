d4d77de64a5ea666983a05006663b4433f01143f
Kumari Anisha <kumari.anisha.dnb34@gmail.com>
2025-04-29 13:14:14 +0530

    updated

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -1,6 +1,7 @@
 const AuthImagePattern = ({ title, subtitle }) => {
   return (
-    <div className="hidden lg:flex items-center justify-center bg-base-200 p-12 min-h-screen">
+    <div className="hidden lg:flex items-center justify-center bg-base-200 p-12 h-full">
+
       <div className="max-w-md text-center">
         <div className="grid grid-cols-3 gap-4 mb-8">
           {[...Array(9)].map((_, i) => (
