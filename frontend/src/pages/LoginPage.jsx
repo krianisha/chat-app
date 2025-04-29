abe4fa5df9fe8af4fa5271dd445e70420d7b2ff9
Kumari Anisha <kumari.anisha.dnb34@gmail.com>
2025-04-29 13:18:04 +0530

    updated

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare } from "lucide-react";
 
 const LoginPage = () => {
-  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false); //hi
   const [formData, setFormData] = useState({
     email: "",
     password: "",
